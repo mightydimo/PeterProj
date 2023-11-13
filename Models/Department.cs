5df4143177850cfd2aaa15528549bf411f1140bf
mightydimo <p_dimo@hotmail.com>
2023-11-13 12:38:00 +1100

    final

---
+++ Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeterProj.Models;
+
+public partial class Department
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+        [System.Text.Json.Serialization.JsonIgnore]
+    public virtual ICollection<Person> People { get; set; } = new List<Person>();
+  
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using PeterProj.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// After Add services to the container.
+builder.Services.AddDbContext<RoiDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// / Define CORS policies to allow access to our API
+builder.Services.AddCors(options => {
+
+    // Build a default CORS policy
+    options.AddDefaultPolicy(policy => {
+
+        // Allow ANY origin to access our API
+        policy.AllowAnyOrigin();
+
+        // Allow SPECIFIC origins to access our API
+        //policy.WithOrigins("http://localhost:19006", "https://app.roi.com.au");
+
+        // Allow any HTTP header
+        policy.AllowAnyHeader();
+
+        // Allow any HTTP method
+        policy.AllowAnyMethod();
+
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,8 +46,17 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Enable CORS (default policy)
+app.UseCors();
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+// Add after RoiDatabaseContext Service configuration
+
+// Add between app.UseHttpsRedirection();  and app.UseAuthorization();
+
+// Enable CORS (default policy)
+app.UseCors();
